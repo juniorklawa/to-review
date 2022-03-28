09b79d393a9bebdec9a0e8c090c95fc368109f4b
everaldo@bb-8 <juniorklawa10@gmail.com>
2022-03-28 15:36:49 -0300

    primitive types

---
+++ src/13 - primitive types/primitive_types.ts
@@ -0,0 +1,38 @@
+// Javascript Data Types are divided in: Primitive and complex
+
+// The primitive (single value) types are:
+// 1. null
+// 2. undefined
+// 3. boolean
+// 4. number
+// 5. string
+// 6. symbol
+
+// And the complex types are:
+// 1. object
+// 2. array
+// 3. function
+// 4. class
+
+
+// UNDEFINED
+// Represents a lack of existence (you should not set a variable to this.)
+
+// NULL
+// Represents lack of existence (you can set a varible to this)
+
+// Booleam
+// true or false
+
+// Number
+// A floating point number (always have some decimals) and that can make math weird.
+
+// String
+// A sequence of characters
+
+// Symbol
+// A unique value that can be used as a property name
+
+
+
+
