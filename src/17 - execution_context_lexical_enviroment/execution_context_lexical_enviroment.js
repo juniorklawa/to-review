63b62f0063e3fa04b92d4c9d83facd934dee1996
everaldo@bb-8 <juniorklawa10@gmail.com>
2022-04-09 18:00:13 -0300

    execution context and lexical enviroment

--- src/17 - execution_context_lexical_enviroment/execution_context_lexical_enviroment.js
+++ src/17 - execution_context_lexical_enviroment/execution_context_lexical_enviroment.js
@@ -14,6 +14,7 @@ function global() {
   // if JS sees a "()" it will create a new execution context and it runs our code
   // also it creates a new lexical environment
   // so each function has its own scope (or which lexical envoriment is currently running)
+  // with the available variables and functions
   function sayMyName() {
     return findName();
   }
