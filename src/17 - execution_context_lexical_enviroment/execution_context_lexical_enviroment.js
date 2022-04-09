b8a39003f9729b1b31fe4903e81cb4a9d81f6b2f
everaldo@bb-8 <juniorklawa10@gmail.com>
2022-04-09 17:59:36 -0300

    chore: versionCode

---
+++ src/17 - execution_context_lexical_enviroment/execution_context_lexical_enviroment.js
@@ -0,0 +1,22 @@
+// the first execution context is the global execution context and it has.
+// 1. a global object
+// 2. "this" (this === window === globalObject)
+
+function global() {
+  function printName() {
+    return "John";
+  }
+
+  function findName() {
+    return printName();
+  }
+
+  // if JS sees a "()" it will create a new execution context and it runs our code
+  // also it creates a new lexical environment
+  // so each function has its own scope (or which lexical envoriment is currently running)
+  function sayMyName() {
+    return findName();
+  }
+
+  sayMyName();
+}
